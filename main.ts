72b9099b96b4234326ddb062b9112e4ea7b959ec
remote1058 <120849740+remote1058@users.noreply.github.com>
2022-12-18 16:29:41 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -9,20 +9,25 @@ bluetooth.onBluetoothDisconnected(function () {
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
     cmd = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
     if (cmd == "F") {
-    	
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 14)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 14)
     }
     if (cmd == "B") {
-    	
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 14)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 14)
     }
     if (cmd == "L") {
-    	
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 14)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 14)
     }
     if (cmd == "R") {
-    	
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 14)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 14)
     }
     if (cmd == "S") {
-    	
+        maqueen.motorStop(maqueen.Motors.M1)
     }
+    bluetooth.uartWriteNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
 })
 basic.forever(function () {
 	
